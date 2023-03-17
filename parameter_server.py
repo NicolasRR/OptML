13b2fddb9884da8274a00427eb61a9a944880c42
NicolasRR <nreateguir@gmail.com>
2023-03-17 19:35:20 +0100

    adapted-to_mnist

--- parameter_server.py
+++ parameter_server.py
@@ -9,15 +9,16 @@ import torch.nn as nn
 from torch import optim
 
 import torchvision
+import argparse
 
 
 BATCH_SIZE = 20
 image_w = 64
 image_h = 64
-num_classes = 30
+num_classes = 10
 BATCH_UPDATE_SIZE = 5
-num_batches = 6
-
+NUM_BATCHES = 6
+DEVICE = "cpu"
 
 def timed_log(text):
     print(f"{datetime.now().strftime('%H:%M:%S')} {text}")
@@ -25,28 +26,54 @@ def timed_log(text):
 TRAIN_LOADER = torch.utils.data.DataLoader(torchvision.datasets.MNIST('../data/mnist_data', 
                                                 download=True, 
                                                 train=True,
-                                                transform=transforms.Compose([
-                                                    transforms.ToTensor(), # first, convert image to PyTorch tensor
-                                                    transforms.Normalize((0.1307,), (0.3081,)) # normalize inputs
+                                                transform=torchvision.transforms.Compose([
+                                                    torchvision.transforms.ToTensor(), # first, convert image to PyTorch tensor
+                                                    torchvision.transforms.Normalize((0.1307,), (0.3081,)) # normalize inputs
                                                 ])), 
                                 batch_size=BATCH_SIZE, 
                                 shuffle=True)
 TEST_LOADER = torch.utils.data.DataLoader(torchvision.datasets.MNIST('../data/mnist_data', 
                                                     download=True, 
                                                     train=False,
-                                                    transform=transforms.Compose([
-                                                        transforms.ToTensor(), # first, convert image to PyTorch tensor
-                                                        transforms.Normalize((0.1307,), (0.3081,)) # normalize inputs
+                                                    transform=torchvision.transforms.Compose([
+                                                        torchvision.transforms.ToTensor(), # first, convert image to PyTorch tensor
+                                                        torchvision.transforms.Normalize((0.1307,), (0.3081,)) # normalize inputs
                                                     ])), 
                                     batch_size=BATCH_SIZE, 
                                     shuffle=True)
 
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
 
+        self.conv1 = nn.Conv2d(1, 32, 3, 1).to(DEVICE)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1).to(DEVICE)
+        self.dropout1 = nn.Dropout2d(0.25).to(DEVICE)
+        self.dropout2 = nn.Dropout2d(0.5).to(DEVICE)
+        self.fc1 = nn.Linear(9216, 128).to(DEVICE)
+        self.fc2 = nn.Linear(128, 10).to(DEVICE)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = nn.functional.relu(x)
+        x = self.conv2(x)
+        x = nn.functional.max_pool2d(x, 2)
+
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+
+
+        x = self.fc1(x)
+        x = nn.functional.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = nn.functional.log_softmax(x, dim=1)
+        return output
 
 class BatchUpdateParameterServer(object):
 
     def __init__(self, batch_update_size=BATCH_UPDATE_SIZE):
-        self.model = torchvision.models.resnet50(num_classes=num_classes)
+        self.model = Net()
         self.lock = threading.Lock()
         self.future_model = torch.futures.Future()
         self.batch_update_size = batch_update_size
@@ -87,20 +114,16 @@ class Trainer(object):
     def __init__(self, ps_rref):
         self.ps_rref = ps_rref
         self.loss_fn = nn.MSELoss()
-        self.one_hot_indices = torch.LongTensor(BATCH_SIZE) \
-                                    .random_(0, num_classes) \
-                                    .view(BATCH_SIZE, 1)
+
 
     def get_next_batch(self):
-        for _ in range(num_batches):
-            inputs = torch.randn(BATCH_SIZE, 3, image_w, image_h)
-            labels = torch.zeros(BATCH_SIZE, num_classes) \
-                        .scatter_(1, self.one_hot_indices, 1)
-            yield inputs.cpu(), labels.cpu()
+        for i, (inputs,labels) in enumerate(TRAIN_LOADER):
+
+            yield inputs.to(DEVICE), labels.to(DEVICE)
 
     def train(self):
         name = rpc.get_worker_info().name
-        m = self.ps_rref.rpc_sync().get_model().cpu()
+        m = self.ps_rref.rpc_sync().get_model().to(DEVICE)
         for inputs, labels in self.get_next_batch():
             timed_log(f"{name} processing one batch")
             self.loss_fn(m(inputs), labels).backward()
@@ -109,7 +132,7 @@ class Trainer(object):
                 self.ps_rref.owner(),
                 BatchUpdateParameterServer.update_and_fetch_model,
                 args=(self.ps_rref, [p.grad for p in m.cpu().parameters()]),
-            ).cpu()
+            ).to(DEVICE)
             timed_log(f"{name} got updated model")
 
 
@@ -132,10 +155,9 @@ def run_ps(trainers):
 
 
 def run(rank, world_size):
-    os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '29500'
+
     options=rpc.TensorPipeRpcBackendOptions(
-        num_worker_threads=16,
+        num_worker_threads=12,
         rpc_timeout=0  # infinite timeout
      )
     if rank != 0:
@@ -160,5 +182,31 @@ def run(rank, world_size):
 
 
 if __name__=="__main__":
-    world_size = BATCH_UPDATE_SIZE + 1
+
+    parser = argparse.ArgumentParser(
+        description="Asynchronous-parameter-Server RPC based training")
+    parser.add_argument(
+        "--world_size",
+        type=int,
+        default=6,
+        help="""Total number of participating processes. Should be the sum of
+        master node and all training nodes.""")
+    parser.add_argument(
+        "--master_addr",
+        type=str,
+        default="localhost",
+        help="""Address of master, will default to localhost if not provided.
+        Master must be able to accept network traffic on the address + port.""")
+    parser.add_argument(
+        "--master_port",
+        type=str,
+        default="29500",
+        help="""Port that master is listening on, will default to 29500 if not
+        provided. Master must be able to accept network traffic on the host and port.""")
+
+    args = parser.parse_args()
+    os.environ['MASTER_ADDR'] = args.master_addr
+    os.environ["MASTER_PORT"] = args.master_port
+    world_size = args.world_size
+    BATCH_UPDATE_SIZE = world_size-1
     mp.spawn(run, args=(world_size, ), nprocs=world_size, join=True)
