d114839daf9e24b0522521a6eeebce54fa607d5e
Arthurepfl123 <48352052+Arthurepfl123@users.noreply.github.com>
2023-03-31 23:51:09 +0200

    normalized all 4 datasets

--- dnn_sync_train.py
+++ dnn_sync_train.py
@@ -244,17 +244,17 @@ def run_worker(ps_rref, train_loader, logger, epochs, worker_accuracy):
 def create_worker_trainloaders(nb_workers, dataset_name, split_dataset, digits_mode, train_split, batch_size, model_accuracy):
     
     if dataset_name == "mnist":
-        train_data = torchvision.datasets.MNIST('data/', 
+        mnist_train = torchvision.datasets.MNIST('data/', 
                                         download=True, 
                                         train=True,
                                         transform=torchvision.transforms.Compose([torchvision.transforms.ToTensor()]))
         
-        trainloader = torch.utils.data.DataLoader(train_data, batch_size=len(train_data))
+        trainloader = torch.utils.data.DataLoader(mnist_train, batch_size=len(mnist_train))
         data = next(iter(trainloader))[0]
         mean = data.mean()
         std = data.std()
 
-        train_data = torchvision.datasets.MNIST('data/', 
+        mnist_train = torchvision.datasets.MNIST('data/', 
                                         download=True, 
                                         train=True,
                                         transform=torchvision.transforms.Compose([
@@ -262,8 +262,8 @@ def create_worker_trainloaders(nb_workers, dataset_name, split_dataset, digits_m
                                         torchvision.transforms.Normalize((mean.item(),), (std.item(),))]))
 
         # Shuffle and split the train_data
-        train_data_indices = torch.randperm(len(train_data))
-        train_length = int(train_split * len(train_data))
+        train_data_indices = torch.randperm(len(mnist_train))
+        train_length = int(train_split * len(mnist_train))
         subsample_train_indices = train_data_indices[:train_length]
 
         if batch_size is None:
@@ -274,7 +274,7 @@ def create_worker_trainloaders(nb_workers, dataset_name, split_dataset, digits_m
             exit()
 
         if not split_dataset and not digits_mode: #workers sharing samples
-            train_loader  = DataLoader(train_data, batch_size=batch_size, sampler=SubsetRandomSampler(subsample_train_indices)) 
+            train_loader  = DataLoader(mnist_train, batch_size=batch_size, sampler=SubsetRandomSampler(subsample_train_indices)) 
             if model_accuracy:
                 train_loader_full= train_loader
                 return (train_loader, train_loader_full)
@@ -283,9 +283,9 @@ def create_worker_trainloaders(nb_workers, dataset_name, split_dataset, digits_m
         
         elif split_dataset and not digits_mode:
             worker_indices = subsample_train_indices.chunk(nb_workers) # Split the train_indices based on the number of workers (world_size - 1)
-            split_dataset_dataloaders = [DataLoader(train_data, batch_size=batch_size, sampler=SubsetRandomSampler(worker_idx)) for worker_idx in worker_indices]
+            split_dataset_dataloaders = [DataLoader(mnist_train, batch_size=batch_size, sampler=SubsetRandomSampler(worker_idx)) for worker_idx in worker_indices]
             if model_accuracy:
-                train_loader_full= DataLoader(train_data, batch_size=batch_size, sampler=SubsetRandomSampler(subsample_train_indices)) 
+                train_loader_full= DataLoader(mnist_train, batch_size=batch_size, sampler=SubsetRandomSampler(subsample_train_indices)) 
                 return (split_dataset_dataloaders, train_loader_full)
             else:
                 return split_dataset_dataloaders
@@ -293,7 +293,7 @@ def create_worker_trainloaders(nb_workers, dataset_name, split_dataset, digits_m
         elif digits_mode:
             digit_indices = {i: [] for i in range(10)}
             for idx in subsample_train_indices:
-                digit = train_data[idx][1]
+                digit = mnist_train[idx][1]
                 digit_indices[digit].append(idx)
             worker_indices = [[] for _ in range(nb_workers)]
             available_digits = list(range(10))
@@ -311,26 +311,69 @@ def create_worker_trainloaders(nb_workers, dataset_name, split_dataset, digits_m
             for i, worker in enumerate(worker_indices):
                 worker_indices[i] = worker[:min_subset_length]
 
-            digit_train_loaders = [DataLoader(train_data, batch_size=batch_size, sampler=SubsetRandomSampler(worker_idx)) for worker_idx in worker_indices]
+            digit_train_loaders = [DataLoader(mnist_train, batch_size=batch_size, sampler=SubsetRandomSampler(worker_idx)) for worker_idx in worker_indices]
 
             if model_accuracy:
                 full_digits_list = []
                 for sublist in worker_indices:
                     full_digits_list.extend(sublist)
-                train_loader_full= DataLoader(train_data, batch_size=batch_size, sampler=SubsetRandomSampler(full_digits_list)) 
+                train_loader_full= DataLoader(mnist_train, batch_size=batch_size, sampler=SubsetRandomSampler(full_digits_list)) 
                 return (digit_train_loaders, train_loader_full)
             else:
                 return digit_train_loaders
         
         elif dataset_name == "fashion_mnist":
+            fashion_mnist_train = torchvision.datasets.FashionMNIST(root='./data', train=True, download=True, transform=torchvision.transforms.Compose([torchvision.transforms.ToTensor()]))
+
+            trainloader = torch.utils.data.DataLoader(fashion_mnist_train, batch_size=len(fashion_mnist_train))
+            data = next(iter(trainloader))[0]
+            mean = data.mean()
+            std = data.std()
+
+            fashion_mnist_train = torchvision.datasets.MNIST('data/', 
+                                            download=True, 
+                                            train=True,
+                                            transform=torchvision.transforms.Compose([
+                                            torchvision.transforms.ToTensor(),
+                                            torchvision.transforms.Normalize((mean.item(),), (std.item(),))]))
+
+
             print("fashion mnist trainloaders not implemented yet")
             exit()
         
         elif dataset_name == "cifar10":
+            cifar10_train = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=torchvision.transforms.Compose([torchvision.transforms.ToTensor()]))
+
+            trainloader = torch.utils.data.DataLoader(cifar10_train, batch_size=len(cifar10_train))
+            data = next(iter(trainloader))[0]
+            mean = data.mean()
+            std = data.std()
+
+            cifar10_train = torchvision.datasets.MNIST('data/', 
+                                            download=True, 
+                                            train=True,
+                                            transform=torchvision.transforms.Compose([
+                                            torchvision.transforms.ToTensor(),
+                                            torchvision.transforms.Normalize((mean.item(),), (std.item(),))]))
+            
             print("cifar10 trainloaders not implemented yet")
             exit()
 
         elif dataset_name == "cifar100":
+            cifar100_train = torchvision.datasets.CIFAR100(root='./data', train=True, download=True, transform=torchvision.transforms.Compose([torchvision.transforms.ToTensor()]))
+
+            trainloader = torch.utils.data.DataLoader(cifar100_train, batch_size=len(cifar100_train))
+            data = next(iter(trainloader))[0]
+            mean = data.mean()
+            std = data.std()
+
+            cifar100_train = torchvision.datasets.MNIST('data/', 
+                                            download=True, 
+                                            train=True,
+                                            transform=torchvision.transforms.Compose([
+                                            torchvision.transforms.ToTensor(),
+                                            torchvision.transforms.Normalize((mean.item(),), (std.item(),))]))
+
             print("cifar100 mnist trainloaders not implemented yet")
             exit()
 
