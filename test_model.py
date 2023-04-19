99f39d64ea24b9b2c0d0d68b32714d2a7901e9cb
Arthurepfl123 <48352052+Arthurepfl123@users.noreply.github.com>
2023-04-19 17:20:39 +0200

    optimized

--- test_model.py
+++ test_model.py
@@ -36,7 +36,6 @@ def main(model_path, batch_size):
 
     with torch.no_grad():
         for data, target in test_loader:
-            data, target = data, target
             output = model(data)
             test_loss += F.nll_loss(output, target, reduction="sum").item()
             pred = output.argmax(dim=1, keepdim=True)
@@ -66,7 +65,9 @@ if __name__ == "__main__":
         help="""Batch size of Mini batch SGD [1,len(train set)].""",
     )
     parser.add_argument(
-        "remainder", nargs=argparse.REMAINDER, help="""Path of the trained model."""
+        "model_path",
+        type=argparse.FileType("r"),
+        help="""Path of the trained model."""
     )
 
     args = parser.parse_args()
@@ -78,16 +79,4 @@ if __name__ == "__main__":
         print("Forbidden value !!! batch_size must be between [1,len(train set)]")
         exit()
 
-    model_path = None
-    for arg in args.remainder:
-        if arg.startswith("--"):
-            continue
-        else:
-            model_path = arg
-            break
-
-    if model_path is None:
-        print("Missing model path !!!")
-        exit()
-
-    main(model_path, args.batch_size)
+    main(args.model_path.name, args.batch_size)
