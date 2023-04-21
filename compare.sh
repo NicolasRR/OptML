4b1d53600a705007739dda8a8b43e9e80e36f424
Arthurepfl123 <48352052+Arthurepfl123@users.noreply.github.com>
2023-04-21 02:07:41 +0200

    bash script working with nb

--- compare.sh
+++ compare.sh
@@ -3,15 +3,30 @@
 #dos2unix compare.sh
 #bash compare.sh
 
-python3 nn_train.py --train_split 0.5 --epoch 2 --dataset mnist
-sleep 0.1
-echo
-python3 dnn_sync_train.py --world_size 3 --train_split 0.5 --epoch 2 --dataset mnist
-sleep 0.1
-echo
-python3 dnn_async_train.py --world_size 3 --train_split 0.5 --epoch 2 --dataset mnist
-sleep 0.1
-echo
-papermill Compare.ipynb Compare.ipynb -p model_classic mnist_classic_05_0001_00_32_2.pt -p model_sync mnist_sync_3_05_0001_00_32_2.pt -p model_async mnist_async_3_05_0001_00_32_2.pt
+train_split=0.5
+epoch=2
+world_size=3
+lr=0.001
+momentum=0.0
+batch_size=32
+dataset="mnist"
 
-#jupyter nbconvert --to notebook --execute my_notebook.ipynb --ExecutePreprocessor.timeout=-1 --output temp.ipynb --allow-errors --Application.log_level=0 --Application.verbose=False --input_file path/to/input/file.csv
+# Format the model filenames based on input parameters
+formatted_train_split=$(echo $train_split | tr -d '.')
+formatted_lr=$(echo $lr | tr -d '.')
+formatted_momentum=$(echo $momentum | tr -d '.')
+model_classic="mnist_classic_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epoch}.pt"
+model_sync="mnist_sync_${world_size}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epoch}.pt"
+model_async="mnist_async_${world_size}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epoch}.pt"
+
+python3 nn_train.py --train_split $train_split --epoch $epoch --dataset $dataset --lr $lr --momentum $momentum --batch_size $batch_size --seed
+sleep 0.1
+echo
+python3 dnn_sync_train.py --world_size 3 --train_split $train_split --epoch $epoch --dataset $dataset --lr $lr --momentum $momentum --batch_size $batch_size --seed 
+sleep 0.1
+echo
+python3 dnn_async_train.py --world_size 3 --train_split $train_split --epoch $epoch --dataset $dataset --lr $lr --momentum $momentum --batch_size $batch_size --seed
+sleep 0.1
+echo
+
+papermill Compare.ipynb Compare.ipynb -p model_classic $model_classic -p model_sync $model_sync -p model_async $model_async
