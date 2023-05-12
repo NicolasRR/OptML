58a4488d671e3aa1ceef1e0eecf38f682591907b
Arthurepfl123 <48352052+Arthurepfl123@users.noreply.github.com>
2023-05-13 00:43:31 +0200

    kfold updates

--- run_kfold.sh
+++ run_kfold.sh
@@ -3,16 +3,34 @@
 if [ "$1" == "--alr" ]
 then
     {
+        echo "Using Adam as optimizer."
         python3 kfold.py --dataset mnist --alr
+        sleep 0.1
+        echo
         python3 kfold.py --dataset fashion_mnist --alr
+        sleep 0.1
+        echo
         # python3 kfold.py --dataset cifar10 --alr
+        # sleep 0.1
+        # echo
         # python3 kfold.py --dataset cifar100 --alr
-    } &> kfold_alr.txt
+        # sleep 0.1
+        # echo
+    } | tee kfold_alr.txt
 else
     {
+        echo "Using SGD as optimizer."
         python3 kfold.py --dataset mnist
+        sleep 0.1
+        echo
         python3 kfold.py --dataset fashion_mnist
+        sleep 0.1
+        echo
         # python3 kfold.py --dataset cifar10
+        # sleep 0.1
+        # echo
         # python3 kfold.py --dataset cifar100
-    } &> kfold.txt
+        # sleep 0.1
+        # echo
+    } | tee kfold.txt # |& tee kfold_alr.txt # for stderr capture
 fi
