9346c9ed05b7be3e937d0adaf1f9f11465320d67
Arthurepfl123 <48352052+Arthurepfl123@users.noreply.github.com>
2023-05-14 16:31:34 +0200

    Update models_size.py

--- models_size.py
+++ models_size.py
@@ -1,6 +1,5 @@
 from common import _get_model, LOSS_FUNC
 
-
 def print_model_params(dataset_name, alt_model):
     model = _get_model(dataset_name, LOSS_FUNC, alt_model)
     total_params = sum(p.numel() for p in model.parameters())
