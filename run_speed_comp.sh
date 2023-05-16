2e552767f87e2156d089e221e494d588a09d21a1
Arthurepfl123 <48352052+Arthurepfl123@users.noreply.github.com>
2023-05-16 18:32:03 +0200

    Create run_speed_comp.sh

---
+++ run_speed_comp.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+epochs=6
+lr=0.01 # maybe need another lr for alt model: 1e-3
+momentum=0.9
+batch_size=100
+world_size_1=4
+world_size_2=7
+
+{
+    # non sync 
+    python3 nn_train.py --dataset fashion_mnist --no_save_model --model_accuracy --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs
+    sleep 0.1
+    echo
+    python3 nn_train.py --dataset fashion_mnist --no_save_model --model_accuracy --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --alt_model
+    sleep 0.1
+    echo
+    # sync
+    python3 dnn_sync_train.py --dataset fashion_mnist --no_save_model --model_accuracy --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs
+    sleep 0.1
+    echo
+    python3 dnn_sync_train.py --dataset fashion_mnist --no_save_model --model_accuracy --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset
+    sleep 0.1
+    echo
+    python3 dnn_sync_train.py --dataset fashion_mnist --no_save_model --model_accuracy --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --alt_model
+    sleep 0.1
+    echo
+    python3 dnn_sync_train.py --dataset fashion_mnist --no_save_model --model_accuracy --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset --alt_model
+    sleep 0.1
+    echo
+    # sync up world_size
+    python3 dnn_sync_train.py --dataset fashion_mnist --no_save_model --model_accuracy --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs
+    sleep 0.1
+    echo
+    python3 dnn_sync_train.py --dataset fashion_mnist --no_save_model --model_accuracy --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset
+    sleep 0.1
+    echo
+    python3 dnn_sync_train.py --dataset fashion_mnist --no_save_model --model_accuracy --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --alt_model
+    sleep 0.1
+    echo
+    python3 dnn_sync_train.py --dataset fashion_mnist --no_save_model --model_accuracy --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset --alt_model
+    sleep 0.1
+    echo
+    # async
+    python3 dnn_async_train.py --dataset fashion_mnist --no_save_model --model_accuracy --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs
+    sleep 0.1
+    echo
+    python3 dnn_async_train.py --dataset fashion_mnist --no_save_model --model_accuracy --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset
+    sleep 0.1
+    echo
+    python3 dnn_async_train.py --dataset fashion_mnist --no_save_model --model_accuracy --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --alt_model
+    sleep 0.1
+    echo
+    python3 dnn_async_train.py --dataset fashion_mnist --no_save_model --model_accuracy --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset --alt_model
+    sleep 0.1
+    echo
+    # async up world_size
+    python3 dnn_async_train.py --dataset fashion_mnist --no_save_model --model_accuracy --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs
+    sleep 0.1
+    echo
+    python3 dnn_async_train.py --dataset fashion_mnist --no_save_model --model_accuracy --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset
+    sleep 0.1
+    echo
+    python3 dnn_async_train.py --dataset fashion_mnist --no_save_model --model_accuracy --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --alt_model
+    sleep 0.1
+    echo
+    python3 dnn_async_train.py --dataset fashion_mnist --no_save_model --model_accuracy --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset --alt_model
+} 2>&1 | tee speed_comparison.txt
