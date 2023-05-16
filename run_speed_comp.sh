efb8e231c7ca8d72bb26bfa990eb31ca0db97c96
Arthurepfl123 <48352052+Arthurepfl123@users.noreply.github.com>
2023-05-16 22:23:30 +0200

    renaming folders

--- run_speed_comp.sh
+++ run_speed_comp.sh
@@ -6,61 +6,62 @@ momentum=0.9
 batch_size=100
 world_size_1=4
 world_size_2=7
+subfolder_name="Speed_results"
 
 # non sync 
-python3 nn_train.py --dataset fashion_mnist --model_accuracy --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs
+python3 nn_train.py --dataset fashion_mnist --model_accuracy --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder_name
 sleep 0.1
 echo
-python3 nn_train.py --dataset fashion_mnist --model_accuracy --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --alt_model
+python3 nn_train.py --dataset fashion_mnist --model_accuracy --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --alt_model --subfolder $subfolder_name
 sleep 0.1
 echo
 # sync
-python3 dnn_sync_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs
+python3 dnn_sync_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder_name
 sleep 0.1
 echo
-python3 dnn_sync_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset
+python3 dnn_sync_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset --subfolder $subfolder_name
 sleep 0.1
 echo
-python3 dnn_sync_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --alt_model
+python3 dnn_sync_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --alt_model --subfolder $subfolder_name
 sleep 0.1
 echo
-python3 dnn_sync_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset --alt_model
+python3 dnn_sync_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset --alt_model --subfolder $subfolder_name
 sleep 0.1
 echo
 # sync up world_size
-python3 dnn_sync_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs
+python3 dnn_sync_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder_name
 sleep 0.1
 echo
-python3 dnn_sync_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset
+python3 dnn_sync_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset --subfolder $subfolder_name
 sleep 0.1
 echo
-python3 dnn_sync_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --alt_model
+python3 dnn_sync_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --alt_model --subfolder $subfolder_name
 sleep 0.1
 echo
-python3 dnn_sync_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset --alt_model
+python3 dnn_sync_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset --alt_model --subfolder $subfolder_name
 sleep 0.1
 echo
 # async
-python3 dnn_async_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs
+python3 dnn_async_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder_name
 sleep 0.1
 echo
-python3 dnn_async_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset
+python3 dnn_async_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset --subfolder $subfolder_name
+sleep 0.1
+echo 
+python3 dnn_async_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --alt_model --subfolder $subfolder_name
 sleep 0.1
 echo
-python3 dnn_async_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --alt_model
-sleep 0.1
-echo
-python3 dnn_async_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset --alt_model
+python3 dnn_async_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset --alt_model --subfolder $subfolder_name
 sleep 0.1
 echo
 # async up world_size
-python3 dnn_async_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs
+python3 dnn_async_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder_name
 sleep 0.1
 echo
-python3 dnn_async_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset
+python3 dnn_async_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset --subfolder $subfolder_name
 sleep 0.1
 echo
-python3 dnn_async_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --alt_model
+python3 dnn_async_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --alt_model --subfolder $subfolder_name
 sleep 0.1
 echo
-python3 dnn_async_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset --alt_model
+python3 dnn_async_train.py --dataset fashion_mnist --model_accuracy --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset --alt_model --subfolder $subfolder_name
