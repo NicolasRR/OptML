a16fda658d2d8a8af15d4642143448d0a5a325ab
Arthurepfl123 <48352052+Arthurepfl123@users.noreply.github.com>
2023-05-26 19:13:19 +0200

    Update run_speed_comp.sh

--- run_speed_comp.sh
+++ run_speed_comp.sh
@@ -12,16 +12,13 @@ formatted_train_split=$(echo $train_split | tr -d '.')
 formatted_lr=$(echo $lr | tr -d '.')
 formatted_momentum=$(echo $momentum | tr -d '.')
 
-# test set to add
-# loss landscape in other script (first ran manually with python3 loss_landscape )
 
 # non sync 
 python3 nn_train.py --dataset fashion_mnist --model_accuracy --seed --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder_name --saves_per_epoch 3
 sleep 0.1
 echo
-# CAREFUL IF USE --split_dataset or --split_labels add sufix: _split_dataset or _split_labels, also change _sync_ for _async_ after dadtaset before worldsize
-model_sync="${subfolder}/${dataset}_sync_${world_size}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epoch}.pt" 
-python3 test_model.py $model_sync --classification_report --training_time --pics --subfolder $subfolder_name
+model_name="${subfolder}/${dataset}_classic_0_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epoch}.pt" 
+python3 test_model.py $model_name --classification_report --training_time --pics --subfolder $subfolder_name
 sleep 0.1
 echo
 
@@ -29,27 +26,66 @@ echo
 python3 dnn_sync_train.py --dataset fashion_mnist --model_accuracy --seed --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder_name --saves_per_epoch 3
 sleep 0.1
 echo
+model_name="${subfolder}/${dataset}_sync_${world_size_1}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epoch}.pt" 
+python3 test_model.py $model_name --classification_report --training_time --pics --subfolder $subfolder_name
+sleep 0.1
+echo
+
 python3 dnn_sync_train.py --dataset fashion_mnist --model_accuracy --seed --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset --subfolder $subfolder_name --saves_per_epoch 3
 sleep 0.1
 echo
+model_name="${subfolder}/${dataset}_sync_${world_size_1}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epoch}_split_dataset.pt" 
+python3 test_model.py $model_name --classification_report --training_time --pics --subfolder $subfolder_name
+sleep 0.1
+echo
+
 # sync up world_size
 python3 dnn_sync_train.py --dataset fashion_mnist --model_accuracy --seed --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder_name --saves_per_epoch 3
 sleep 0.1
 echo
+model_name="${subfolder}/${dataset}_sync_${world_size_2}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epoch}.pt" 
+python3 test_model.py $model_sync --classification_report --training_time --pics --subfolder $subfolder_name
+sleep 0.1
+echo
+
 python3 dnn_sync_train.py --dataset fashion_mnist --model_accuracy --seed --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset --subfolder $subfolder_name --saves_per_epoch 3
 sleep 0.1
 echo
+model_name="${subfolder}/${dataset}_sync_${world_size_2}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epoch}_split_dataset.pt" 
+python3 test_model.py $model_sync --classification_report --training_time --pics --subfolder $subfolder_name
+sleep 0.1
+echo
+
 # async
 python3 dnn_async_train.py --dataset fashion_mnist --model_accuracy --seed --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder_name --saves_per_epoch 3
 sleep 0.1
 echo
+model_name="${subfolder}/${dataset}_async_${world_size_1}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epoch}.pt" 
+python3 test_model.py $model_name --classification_report --training_time --pics --subfolder $subfolder_name
+sleep 0.1
+echo
+
 python3 dnn_async_train.py --dataset fashion_mnist --model_accuracy --seed --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset --subfolder $subfolder_name --saves_per_epoch 3
 sleep 0.1
-echo 
+echo
+model_name="${subfolder}/${dataset}_async_${world_size_1}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epoch}_split_dataset.pt" 
+python3 test_model.py $model_name --classification_report --training_time --pics --subfolder $subfolder_name
+sleep 0.1
+echo
+
 # async up world_size
 python3 dnn_async_train.py --dataset fashion_mnist --model_accuracy --seed --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder_name --saves_per_epoch 3
 sleep 0.1
 echo
+model_name="${subfolder}/${dataset}_async_${world_size_2}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epoch}.pt" 
+python3 test_model.py $model_name --classification_report --training_time --pics --subfolder $subfolder_name
+sleep 0.1
+echo
+
 python3 dnn_async_train.py --dataset fashion_mnist --model_accuracy --seed --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset --subfolder $subfolder_name --saves_per_epoch 3
+sleep 0.1
+echo
+model_name="${subfolder}/${dataset}_async_${world_size_2}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epoch}_split_dataset.pt" 
+python3 test_model.py $model_name --classification_report --training_time --pics --subfolder $subfolder_name
 sleep 0.1
 echo
