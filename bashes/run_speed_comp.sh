37b1b4ae3e40e4967a9405ab208069029be83fff
NicolasRR <nreateguir@gmail.com>
2023-05-27 18:22:37 +0200

    clean

--- bashes/run_speed_comp.sh
+++ bashes/run_speed_comp.sh
@@ -41,84 +41,17 @@ formatted_momentum=$(echo $momentum | tr -d '.')
 python3 $nn_train_py --dataset $dataset --model_accuracy --seed --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder --saves_per_epoch 3 --val --train_split $train_split
 sleep 0.1
 echo
-model_name="${subfolder}/${dataset}_classic_0_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_model.pt" 
-log_name="${subfolder}/${dataset}_classic_0_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_log.log" 
-python3 $test_model_py $model_name $log_name --classification_report --training_time --pics --subfolder $subfolder
-sleep 0.1
-echo
 
-# sync
-python3 $dnn_sync_train_py --dataset $dataset --model_accuracy --seed --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder --saves_per_epoch 3 --val --train_split $train_split
-sleep 0.1
-echo
-model_name="${subfolder}/${dataset}_sync_${world_size_1}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_model.pt" 
-log_name="${subfolder}/${dataset}_sync_${world_size_1}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_log.log" 
-python3 $test_model_py $model_name $log_name --classification_report --training_time --pics --subfolder $subfolder
-sleep 0.1
-echo
+mode2=""
+mode1="--split_dataset"
+mode3="--split_labels"
 
-python3 $dnn_sync_train_py --dataset $dataset --model_accuracy --seed --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset --subfolder $subfolder --saves_per_epoch 3 --train_split $train_split
-sleep 0.1
-echo
-model_name="${subfolder}/${dataset}_sync_${world_size_1}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_split_dataset_model.pt" 
-log_name="${subfolder}/${dataset}_sync_${world_size_1}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_split_dataset_log.log" 
-python3 $test_model_py $model_name $log_name --classification_report --training_time --pics --subfolder $subfolder
-sleep 0.1
-echo
-
-# sync up world_size
-python3 $dnn_sync_train_py --dataset $dataset --model_accuracy --seed --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder --saves_per_epoch 3 --val --train_split $train_split
-sleep 0.1
-echo
-model_name="${subfolder}/${dataset}_sync_${world_size_2}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_model.pt" 
-log_name="${subfolder}/${dataset}_sync_${world_size_2}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_log.log" 
-python3 $test_model_py $model_name $log_name --classification_report --training_time --pics --subfolder $subfolder
-sleep 0.1
-echo
-
-python3 $dnn_sync_train_py --dataset $dataset --model_accuracy --seed --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset --subfolder $subfolder --saves_per_epoch 3 --train_split $train_split
-sleep 0.1
-echo
-model_name="${subfolder}/${dataset}_sync_${world_size_2}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_split_dataset_model.pt" 
-log_name="${subfolder}/${dataset}_sync_${world_size_2}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_split_dataset_log.log" 
-python3 $test_model_py $model_name $log_name --classification_report --training_time --pics --subfolder $subfolder
-sleep 0.1
-echo
-
-# async
-python3 $dnn_async_train_py --dataset $dataset --model_accuracy --seed --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder --saves_per_epoch 3 --val --train_split $train_split
-sleep 0.1
-echo
-model_name="${subfolder}/${dataset}_async_${world_size_1}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_model.pt" 
-log_name="${subfolder}/${dataset}_async_${world_size_1}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_log.log" 
-python3 $test_model_py $model_name $log_name --classification_report --training_time --pics --subfolder $subfolder
-sleep 0.1
-echo
-
-python3 $dnn_async_train_py --dataset $dataset --model_accuracy --seed --world_size $world_size_1 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset --subfolder $subfolder --saves_per_epoch 3 --train_split $train_split
-sleep 0.1
-echo
-model_name="${subfolder}/${dataset}_async_${world_size_1}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_split_dataset_model.pt" 
-log_name="${subfolder}/${dataset}_async_${world_size_1}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_split_dataset_log.log" 
-python3 $test_model_py $model_name $log_name --classification_report --training_time --pics --subfolder $subfolder
-sleep 0.1
-echo
-
-# async up world_size
-python3 $dnn_async_train_py --dataset $dataset --model_accuracy --seed --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder --saves_per_epoch 3 --val --train_split $train_split
-sleep 0.1
-echo
-model_name="${subfolder}/${dataset}_async_${world_size_2}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_model.pt" 
-log_name="${subfolder}/${dataset}_async_${world_size_2}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_log.log" 
-python3 $test_model_py $model_name $log_name --classification_report --training_time --pics --subfolder $subfolder
-sleep 0.1
-echo
-
-python3 $dnn_async_train_py --dataset $dataset --model_accuracy --seed --world_size $world_size_2 --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --split_dataset --subfolder $subfolder --saves_per_epoch 3 --train_split $train_split
-sleep 0.1
-echo
-model_name="${subfolder}/${dataset}_async_${world_size_2}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_split_dataset_model.pt" 
-log_name="${subfolder}/${dataset}_async_${world_size_2}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_split_dataset_log.log" 
-python3 $test_model_py $model_name $log_name --classification_report --training_time --pics --subfolder $subfolder
-sleep 0.1
-echo
+for file in $dnn_sync_train_py $dnn_async_train_py; do
+  for mode in $mode1 $mode2 $mode3;do
+    for world_size in $world_size_1 $world_size_2; do
+      python3 $file --dataset $dataset --model_accuracy --seed --world_size $world_size --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder --saves_per_epoch 3 --train_split $train_split $mode
+      sleep 0.1
+      echo
+done
+done
+done
