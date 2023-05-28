6b060768600ebe6895c996c079520823eabf863e
NicolasRR <nreateguir@gmail.com>
2023-05-28 10:14:08 +0200

    up

--- bashes/run_tmp.sh
+++ bashes/run_tmp.sh
@@ -1,3 +1,3 @@
 bash run_speed_comp.sh > "../Speed_results/speed_comp.txt"
-bash test_model.sh "../Speed_results/" > "../Speed_results/loss_landscape.txt"
-bash loss_landcape.sh "../Speed_results/" > "../Speed_results/test_model.txt"
+bash test_model.sh "../Speed_results/" > "../Speed_results/test_model.txt"
+bash loss_landcape.sh "../Speed_results/" > "../Speed_results/loss_landscape.txt"
