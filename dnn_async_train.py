d50c6c596eec9da8bc82b8cbe2d97a8582689c06
Arthurepfl123 <48352052+Arthurepfl123@users.noreply.github.com>
2023-05-28 19:43:41 +0200

    resnet18 save model fix

--- dnn_async_train.py
+++ dnn_async_train.py
@@ -106,10 +106,7 @@ class ParameterServer_async(object):
                     self.global_batch_counter / self.nb_workers - 1
                 ) % total_batches_to_run
                 if relative_batch_idx in self.save_idx:
-                    weights = [
-                        w.detach().clone().cpu().numpy()
-                        for w in self.model.parameters()
-                    ]
+                    weights = np.concatenate([w.detach().clone().cpu().numpy().ravel() for w in self.model.state_dict().values()])
                     self.weights_matrix.append(weights)
 
             if self.scheduler is not None or self.val:
--- nn_train.py
+++ nn_train.py
@@ -34,7 +34,7 @@ def run(
     alt_model,
 ):
     loss_func = LOSS_FUNC
-
+    
     model = _get_model(dataset_name, loss_func, alt_model)
     optimizer = get_optimizer(model, learning_rate, momentum, use_alr)
 
@@ -105,9 +105,7 @@ def run(
             )
             if saves_per_epoch is not None:
                 if batch_idx in save_idx:
-                    weights = [
-                        w.detach().clone().cpu().numpy() for w in model.parameters()
-                    ]
+                    weights = np.concatenate([w.detach().clone().cpu().numpy().ravel() for w in model.state_dict().values()])
                     weights_matrix.append(weights)
 
             progress_bar.update(1)
