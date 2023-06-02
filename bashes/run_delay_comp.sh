c56d4509648b870e403d1e3d6f81d92cdd3c8c3e
NicolasRR <nreateguir@gmail.com>
2023-06-02 12:59:51 +0200

    compact?

--- bashes/run_delay_comp.sh
+++ bashes/run_delay_comp.sh
@@ -9,7 +9,10 @@ subfolder="$project_dir/Delay_results"
 
 dataset="fashion_mnist"
 train_split=1
-world_size=4
+world_size1=3
+world_size2=6
+world_size3=11
+
 lr=0.005 
 momentum=0.9
 batch_size=32
@@ -33,133 +36,33 @@ formatted_train_split=$(printf "%.1f\n" $(echo "$train_split * 10" | bc) | tr -d
 formatted_lr=$(echo $lr | tr -d '.')
 formatted_momentum=$(echo $momentum | tr -d '.')
 
-# sync
-python3 $dnn_sync_train_py --dataset $dataset --world_size $world_size --model_accuracy --seed  --val --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder --train_split $train_split --saves_per_epoch 3
-sleep 0.1
-echo
-model_sync="${subfolder}/${dataset}_sync_${world_size}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_model.pt"
-log_sync="${subfolder}/${dataset}_sync_${world_size}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_log.log"
-python3 $test_model_py $model_sync $log_sync --classification_report --training_time --pics --subfolder $subfolder
-sleep 0.1
-echo
+for world_size in $world_size1 $world_size2 $world_size3; do
+  python3 $dnn_sync_train_py --dataset $dataset --world_size $world_size --model_accuracy --seed  --val --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder --train_split $train_split --saves_per_epoch 3
+  python3 $dnn_async_train_py --dataset $dataset --world_size $world_size --model_accuracy --seed  --val --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder --train_split $train_split --saves_per_epoch 3
+done
+delay1="small"
+delay2="medium"
+delay3="long"
+type1="constant"
+type2="gaussian"
 
-# async baseline
-python3 $dnn_async_train_py --dataset $dataset --world_size $world_size --model_accuracy --seed  --val --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder --train_split $train_split --saves_per_epoch 3
-sleep 0.1
-echo
-model_async="${subfolder}/${dataset}_async_${world_size}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_model.pt"
-log_async="${subfolder}/${dataset}_async_${world_size}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_log.log"
-python3 $test_model_py $model_async $log_async --classification_report --training_time --pics --subfolder $subfolder
-sleep 0.1
-echo
 
-# constant delays
-python3 $dnn_async_train_py --dataset $dataset --world_size $world_size --model_accuracy --seed  --val --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder --delay --delay_intensity "small" --delay_type "constant" --train_split $train_split --saves_per_epoch 3
-sleep 0.1
-echo
-model_async="${subfolder}/${dataset}_async_${world_size}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_delay_small_constant_model.pt"
-log_async="${subfolder}/${dataset}_async_${world_size}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_delay_small_constant_log.log"
-python3 $test_model_py $model_async $log_async --classification_report --training_time --pics --subfolder $subfolder
-sleep 0.1
-echo
+for type in $type1 $type2;do
+  for world_size in $world_size1 $world_size2 $world_size3; do
+    for delay in $delay1 $delay2 $delay3; do
+      python3 $dnn_async_train_py --dataset $dataset --world_size $world_size --model_accuracy --seed --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder --train_split $train_split --saves_per_epoch 3 --delay --delay_intensity $delay --delay_type $type --split_labels 
+      sleep 0.1
+      echo
+done
+done
+done
 
-python3 $dnn_async_train_py --dataset $dataset --world_size $world_size --model_accuracy --seed  --val --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder --delay --delay_intensity "medium" --delay_type "constant" --train_split $train_split --saves_per_epoch 3
-sleep 0.1
-echo
-model_async="${subfolder}/${dataset}_async_${world_size}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_delay_medium_constant_model.pt"
-log_async="${subfolder}/${dataset}_async_${world_size}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_delay_medium_constant_log.log"
-python3 $test_model_py $model_async $log_async --classification_report --training_time --pics --subfolder $subfolder
-sleep 0.1
-echo
-
-python3 $dnn_async_train_py --dataset $dataset --world_size $world_size --model_accuracy --seed  --val --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder --delay --delay_intensity "long" --delay_type "constant" --train_split $train_split --saves_per_epoch 3
-sleep 0.1
-echo
-model_async="${subfolder}/${dataset}_async_${world_size}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_delay_long_constant_model.pt"
-log_async="${subfolder}/${dataset}_async_${world_size}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_delay_long_constant_log.log"
-python3 $test_model_py $model_async $log_async --classification_report --training_time --pics --subfolder $subfolder
-sleep 0.1
-echo
-
-# gaussian delays
-python3 $dnn_async_train_py --dataset $dataset --world_size $world_size --model_accuracy --seed  --val --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder --delay --delay_intensity "small" --delay_type "gaussian" --train_split $train_split --saves_per_epoch 3
-sleep 0.1
-echo
-model_async="${subfolder}/${dataset}_async_${world_size}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_delay_small_gaussian_model.pt"
-log_async="${subfolder}/${dataset}_async_${world_size}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_delay_small_gaussian_log.log"
-python3 $test_model_py $model_async $log_async --classification_report --training_time --pics --subfolder $subfolder
-sleep 0.1
-echo
-
-python3 $dnn_async_train_py --dataset $dataset --world_size $world_size --model_accuracy --seed  --val --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder --delay --delay_intensity "medium" --delay_type "gaussian" --train_split $train_split --saves_per_epoch 3
-sleep 0.1
-echo
-model_async="${subfolder}/${dataset}_async_${world_size}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_delay_medium_gaussian_model.pt"
-log_async="${subfolder}/${dataset}_async_${world_size}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_delay_medium_gaussian_log.log"
-python3 $test_model_py $model_async $log_async --classification_report --training_time --pics --subfolder $subfolder
-sleep 0.1
-echo
-python3 $dnn_async_train_py --dataset $dataset --world_size $world_size --model_accuracy --seed  --val --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder --delay --delay_intensity "long" --delay_type "gaussian" --train_split $train_split --saves_per_epoch 3
-sleep 0.1
-echo
-model_async="${subfolder}/${dataset}_async_${world_size}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_delay_long_gaussian_model.pt"
-log_async="${subfolder}/${dataset}_async_${world_size}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_delay_long_gaussian_log.log"
-python3 $test_model_py $model_async $log_async --classification_report --training_time --pics --subfolder $subfolder
-sleep 0.1
-echo
-
-# worker1 constant delay
-python3 $dnn_async_train_py --dataset $dataset --world_size $world_size --model_accuracy --seed  --val --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder --slow_worker_1 --delay_intensity "small" --delay_type "constant" --train_split $train_split --saves_per_epoch 3
-sleep 0.1
-echo
-model_async="${subfolder}/${dataset}_async_${world_size}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_slow_worker_1_small_constant_model.pt"
-log_async="${subfolder}/${dataset}_async_${world_size}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_slow_worker_1_small_constant_log.log"
-python3 $test_model_py $model_async $log_async --classification_report --training_time --pics --subfolder $subfolder
-sleep 0.1
-echo
-
-python3 $dnn_async_train_py --dataset $dataset --world_size $world_size --model_accuracy --seed  --val --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder --slow_worker_1 --delay_intensity "medium" --delay_type "constant" --train_split $train_split --saves_per_epoch 3
-sleep 0.1
-echo
-model_async="${subfolder}/${dataset}_async_${world_size}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_slow_worker_1_medium_constant_model.pt"
-log_async="${subfolder}/${dataset}_async_${world_size}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_slow_worker_1_medium_constant_log.log"
-python3 $test_model_py $model_async $log_async --classification_report --training_time --pics --subfolder $subfolder
-sleep 0.1
-echo
- 
-python3 $dnn_async_train_py --dataset $dataset --world_size $world_size --model_accuracy --seed  --val --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder --slow_worker_1 --delay_intensity "long" --delay_type "constant" --train_split $train_split --saves_per_epoch 3
-sleep 0.1
-echo
-model_async="${subfolder}/${dataset}_async_${world_size}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_slow_worker_1_long_constant_model.pt"
-log_async="${subfolder}/${dataset}_async_${world_size}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_slow_worker_1_long_constant_log.log"
-python3 $test_model_py $model_async $log_async --classification_report --training_time --pics --subfolder $subfolder
-sleep 0.1
-echo
-
-# worker1 gaussian delay
-python3 $dnn_async_train_py --dataset $dataset --world_size $world_size --model_accuracy --seed  --val --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder --slow_worker_1 --delay_intensity "small" --delay_type "gaussian" --train_split $train_split --saves_per_epoch 3
-sleep 0.1
-echo
-model_async="${subfolder}/${dataset}_async_${world_size}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_slow_worker_1_small_gaussian_model.pt"
-log_async="${subfolder}/${dataset}_async_${world_size}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_slow_worker_1_small_gaussian_log.log"
-python3 $test_model_py $model_async $log_async --classification_report --training_time --pics --subfolder $subfolder
-sleep 0.1
-echo
-
-python3 $dnn_async_train_py --dataset $dataset --world_size $world_size --model_accuracy --seed  --val --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder --slow_worker_1 --delay_intensity "medium" --delay_type "gaussian" --train_split $train_split --saves_per_epoch 3
-sleep 0.1
-echo
-model_async="${subfolder}/${dataset}_async_${world_size}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_slow_worker_1_medium_gaussian_model.pt"
-log_async="${subfolder}/${dataset}_async_${world_size}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_slow_worker_1_medium_gaussian_log.log"
-python3 $test_model_py $model_async $log_async --classification_report --training_time --pics --subfolder $subfolder
-sleep 0.1
-echo
-
-python3 $dnn_async_train_py --dataset $dataset --world_size $world_size --model_accuracy --seed  --val --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder --slow_worker_1 --delay_intensity "long" --delay_type "gaussian" --train_split $train_split --saves_per_epoch 3
-sleep 0.1
-echo
-model_async="${subfolder}/${dataset}_async_${world_size}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_slow_worker_1_long_gaussian_model.pt"
-log_async="${subfolder}/${dataset}_async_${world_size}_${formatted_train_split}_${formatted_lr}_${formatted_momentum}_${batch_size}_${epochs}_SGD_spe3_val_slow_worker_1_long_gaussian_log.log"
-python3 $test_model_py $model_async $log_async --classification_report --training_time --pics --subfolder $subfolder
-sleep 0.1
-echo
+for type in $type1 $type2;do
+  for world_size in $world_size1 $world_size2 $world_size3; do
+    for delay in $delay1 $delay2 $delay3; do
+      python3 $dnn_async_train_py --dataset $dataset --world_size $world_size --model_accuracy --seed --lr $lr --momentum $momentum --batch_size $batch_size --epochs $epochs --subfolder $subfolder --train_split $train_split --saves_per_epoch 3 --delay --delay_intensity $delay --delay_type $type --slow_worker_1 --split_labels
+      sleep 0.1
+      echo
+done
+done
+done
